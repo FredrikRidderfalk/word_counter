c888715da62a28dac4632fd2f4a5a0e0fa9b018d
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-10-05 11:57:25 +0200

    everything except paragraph working

--- regex.js
+++ regex.js
@@ -1,55 +1,66 @@
-// Tasks:
-
-// - write the JS so that the word count appears on the screen
-// - update the count as the user types
-
 const userInput = document.querySelector("[name='user-input']");
 const wordCounter = document.querySelector(".word-counter");
 const characterCounter = document.querySelector(".character-counter");
+const paragraphCounter = document.querySelector(".paragraph-counter");
 const longestWordCount = document.querySelector(".longest-word-count");
 const longestWordIdentifyer = document.querySelector(
   ".longest-word-identifyer"
 );
-const languageIdentifyer = document.querySelector(".language-identifyer");
 
 // word counter
 const wordCount = (str) => {
-  return str.split(" ").length;
+  if (userInput.value != "") {
+    return str.match(/(\w+)/g).length;
+  }
 };
 
 // character counter
 const characterCount = (str) => {
-  return str.length;
+  if (userInput.value != "") {
+    return str.match(/(.)/g).length;
+  }
 };
 
 // longest word counter
 const countLongestWord = (str) => {
-  const strSplit = str.split(" ");
-  let longestWord = 0;
-  for (let i = 0; i < strSplit.length; i++) {
-    if (strSplit[i].length > longestWord) {
-      longestWord = strSplit[i].length;
+  if (userInput.value != "") {
+    const strSplit = str.split(" ");
+    let longestWord = 0;
+    for (let i = 0; i < strSplit.length; i++) {
+      if (strSplit[i].length > longestWord) {
+        longestWord = strSplit[i].length;
+      }
     }
+    return longestWord;
   }
-  return longestWord;
 };
+
 countLongestWord(userInput.value);
 
 // longest word identifyer
 function findLongestWord(str) {
-  const strSplit = str.split(" ");
-  let longestWord = strSplit.sort(function (a, b) {
-    return b.length - a.length;
-  });
-  return longestWord[0];
+  if (userInput.value != "") {
+    const strSplit = str.split(" ");
+    let longestWord = strSplit.sort(function (a, b) {
+      return b.length - a.length;
+    });
+    return longestWord[0];
+  }
 }
 
 // paragraph detector
+const paragraphCount = (str) => {
+  if (userInput.value != "") {
+    return str.match(/([.?!])\s*(?=[A-Z])/g).length;
+  }
+};
 
 // event listener for all components
+
 userInput.addEventListener("keyup", () => {
   wordCounter.textContent = wordCount(userInput.value);
   characterCounter.textContent = characterCount(userInput.value);
   longestWordCount.textContent = countLongestWord(userInput.value);
   longestWordIdentifyer.textContent = findLongestWord(userInput.value);
+  //   paragraphCounter.textContent = paragraphCount(userInput.value);
 });
